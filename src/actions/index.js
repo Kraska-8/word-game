9e72dec6cade339e739adc6bbd8a2bd75423192f
kraska-8 <kraska-8@yandex.ru>
2022-01-10 21:43:36 +0300

    Success guess test reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,19 @@
 import axios from 'axios';
 
+export const actionTypes = {
+  CORRECT_GUESS: 'CORRECT_GUESS',
+  GUESS_WORD: 'GUESS_WORD',
+  SET_SECRET_WORD: 'SET_SECRET_WORD',
+};
+
+/**
+ * @function correctGuess
+ * @returns {object} - Action object with type 'CORRECT_GUESS'
+ */
+export const correctGuess = () => {
+  return { type: actionTypes.CORRECT_GUESS };
+}
+
 export const getSecretWord = () => {
   return axios.get('http://localhost:3000')
     .then(response => response.data)
