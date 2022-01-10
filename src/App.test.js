43a2d9ea4e1f70dc7c718841d73e49149045cd08
kraska-8 <kraska-8@yandex.ru>
2022-01-10 21:15:39 +0300

    Mocking axios and useEffect hook tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,15 @@
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { findByTestAttribute} from '../test/testUtils';
 import App from './App';
 
+jest.mock('./actions');
+import { getSecretWord as mockGetSecretWord } from './actions';
 /**
  * Setup function for App component
  * @returns {ShallowWrapper}
  */
 const setup = () => {
-  return shallow(<App />);
+  return mount(<App />);
 };
 
 test('Irenders without errors', () => {
@@ -17,3 +19,21 @@ test('Irenders without errors', () => {
   expect(appComponent).toHaveLength(1);
 });
 
+describe('get secret word', () => {
+  beforeEach(() => {
+    mockGetSecretWord.mockClear();
+  });
+  test('get secret word on app mount', () => {
+    const wrapper = setup();
+
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(1);
+  });
+
+  test('getSecretWord does not run on app update', () => {
+    const wrapper = setup();
+    mockGetSecretWord.mockClear();
+    wrapper.setProps();
+
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(0);
+  });
+});
